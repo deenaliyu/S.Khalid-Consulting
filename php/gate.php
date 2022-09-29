31669dd9b33c4d03afdfd875ac56b8b24d52c4e6
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-09-29 12:01:56 +0100

    good result

--- php/gate.php
+++ php/gate.php
@@ -2,8 +2,8 @@
 
 function fetchProperty(){
     include "config/index.php";
-    $availability = "sale";
-    $query_User_re = sprintf("SELECT * FROM property_L WHERE `availability` = $availability");
+    $availability = 'sale';
+    $query_User_re = sprintf("SELECT * FROM property_l WHERE `availability` = '$availability'");
     $User_re = mysqli_query($sKhalid, $query_User_re) or die(mysqli_error($sKhalid));
     $row_User_re = mysqli_fetch_assoc($User_re);
     $totalRows_User_re = mysqli_num_rows($User_re);
@@ -29,12 +29,12 @@ function createPropertyListing(){
     $bathroom = $_POST['bathroom'];
     $availability = $_POST['availability'];
 
-    $query_User_re = sprintf("INSERT INTO property_L (`listing_type`, `image`, `property_type`, `price`, `description`, `location`, `land_size`, `bed_space`, `bathroom`, `availability`)
-                            VALUES('$listing_type', '$image', '$property_type', '$price', '$description', '$location', '$land_size', '$bed_space', '$bathroom', $availability)");
+    $query_User_re = sprintf("INSERT INTO property_l (`listing_type`, `image`, `property_type`, `price`, `description`, `location`, `land_size`, `bed_space`, `bathroom`, `availability`)
+                            VALUES('$listing_type', '$image', '$property_type', '$price', '$description', '$location', '$land_size', '$bed_space', '$bathroom', '$availability')");
     $User_re = mysqli_query($sKhalid, $query_User_re) or die(mysqli_error($sKhalid));
-    $row_User_re = mysqli_fetch_assoc($User_re);
-    $totalRows_User_re = mysqli_num_rows($User_re);
-    if ($totalRows_User_re > 0) {
+    // $row_User_re = mysqli_fetch_assoc($User_re);
+    // $totalRows_User_re = mysqli_num_rows($User_re);
+    if ($User_re ) {
             $arr = [
                 'status' => 1,'message' => "property added successfully"
             ];
@@ -56,7 +56,7 @@ function updatePropertyListing(){
     $bathroom = $_POST['bathroom'];
     $availability = $_POST['availability'];
 
-    $query_User_re = sprintf("UPDATE property_L SET `listing_type`= '$listing_type', `image` = '$image', `property_type`= '$property_type', `price` = '$price', `description` = '$description', `location` = '$location', `land_size` = '$land_size', `bed_space` = '$bed_space', `bathroom` = '$bathroom', `availability` = '$availability' WHERE id = '$id'");
+    $query_User_re = sprintf("UPDATE property_l SET `listing_type`= '$listing_type', `image` = '$image', `property_type`= '$property_type', `price` = '$price', `description` = '$description', `location` = '$location', `land_size` = '$land_size', `bed_space` = '$bed_space', `bathroom` = '$bathroom', `availability` = '$availability' WHERE id = '$id'");
     $User_re = mysqli_query($sKhalid, $query_User_re) or die(mysqli_error($sKhalid));
     $row_User_re = mysqli_fetch_assoc($User_re);
     $totalRows_User_re = mysqli_num_rows($User_re);
