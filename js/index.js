7db2fddb9374985f8b0416110124a3e8b658e776
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-10-06 11:02:09 +0100

    updates

--- js/index.js
+++ js/index.js
@@ -82,25 +82,11 @@ $('#navBar').html(`
 <div class="collapse navbar-collapse" id="navbarCollapse">
   <div class="navbar-nav ms-auto">
     <a href="index.html" class="nav-item nav-link active">Home</a>
-    <a href="services.html" class="nav-item nav-link">Services</a>
-    <div class="nav-item dropdown">
-      <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Property</a>
-      <div class="dropdown-menu rounded-0 m-0">
-        <a href="property-list.html" class="dropdown-item">Rent</a>
-        <a href="property-list.html" class="dropdown-item">Sale</a>
-        <a href="property-type.html" class="dropdown-item">Property Type</a>
-        <a href="property-agent.html" class="dropdown-item">Property Agent</a>
-      </div>
-    </div>
-    <!-- <div class="nav-item dropdown">
-        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-        <div class="dropdown-menu rounded-0 m-0">
-            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-            <a href="404.html" class="dropdown-item">404 Error</a>
-        </div>
-    </div> -->
-    <a href="about.html" class="nav-item nav-link">About</a>
-    <a href="contact.html" class="nav-item nav-link">Contact</a>
+    <a href="#service-section" class="nav-item nav-link">Services</a>
+    <a href="#property-section" class="nav-link">Property</a>
+
+    <a href="#about-section" class="nav-item nav-link">About</a>
+    <a href="#contact-section" class="nav-item nav-link">Contact</a>
   </div>
 
 </div>
@@ -109,12 +95,12 @@ $('#navBar').html(`
 
 $('#theHeader').html(`
 <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
-  <div class="col-md-6 p-5 mt-lg-5">
+  <div class="col-md-6 p-5" style="margin-top: 8rem">
     <h1 class="display-5 animated fadeIn mb-4">Find A <span class="text-primary">Perfect Home</span> To Live In
     </h1>
-    <p class="animated fadeIn mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet
-      sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-    <a href="property-list.html" class="btn btn-primary py-3 px-5 me-3 animated fadeIn">Get Started</a>
+    <p class="animated fadeIn mb-4 pb-2">Our vision is to build a reputable estate firm where integrity
+     will serve as a key to alasting professionalism.</p>
+    <a href="#property-section" class="btn btn-primary py-3 px-5 me-3 animated fadeIn">Get Started</a>
   </div>
   <div class="col-md-6 animated fadeIn">
     <div class="owl-carousel header-carousel">
@@ -139,7 +125,7 @@ const fetchProperties = async () => {
       <div class="property-item rounded overflow-hidden">
         <div class="position-relative overflow-hidden">
           <img class="img-fluid" src="img/${property.image}" alt="">
-          <div class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">For Rent
+          <div class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">For ${property.availability}
           </div>
           <div class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
             ${property.property_type}</div>
